a37f3734dab1e676315f964aff355c804d47ba03
Saransh Kumar <cryptosingh@gmail.com>
2020-07-22 22:45:33 +0530

    update ScreenShots and Jar file. Now jar can run without installing JavaFX

--- TodoList Jar File/run.bat
+++ TodoList Jar File/run.bat
@@ -1,1 +1,1 @@
-java --module-path "C:\JavaFX\javafx-sdk-11.0.2\lib" --add-modules javafx.controls,javafx.fxml -jar TodoList.jar
+java -jar TodoList.jar
---
+++ src/main.java
@@ -0,0 +1,8 @@
+import com.saransh.todolist.Main;
+
+public class main {
+
+    public static void main(String[] args) {
+        Main.main(args);
+    }
+}
